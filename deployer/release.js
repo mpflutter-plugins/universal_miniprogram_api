b82b925290a55b502984656f05ad00ef95118328
PonyCui <cuis@vip.qq.com>
2021-12-24 15:06:47 +0800

    chore: change currentVersion to 0.0.3

--- deployer/release.js
+++ deployer/release.js
@@ -16,7 +16,7 @@ const cosInstance = new COS({
 const cosBucket = "mpflutter-dist-1253771526";
 const cosRegion = "ap-guangzhou";
 
-const currentVersion = '0.0.2';
+const currentVersion = '0.0.3';
 
 class DartPackageDeployer {
   constructor(name) {
